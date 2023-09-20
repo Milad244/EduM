a399a77579a36eab1001e845c1ef8d754ab3b6c9
Milad244 <milad.abdi244@gmail.com>
2023-09-20 16:25:24 -0700

    Adding entire project to the repo

---
+++ app.js
@@ -0,0 +1,6 @@
+// When the user clicks on the button, scroll to the top of the document
+function scrollDown() {
+  document.body.scrollTop = 1000; // For Safari
+  document.documentElement.scrollTop = 1000; // For Chrome, Firefox, IE and Opera
+}
+
